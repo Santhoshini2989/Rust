b46020aa917f33f2ceafb6e752a682ddf71cc0f0
santhoshini <santhoshinia01@gmail.com>
2025-08-06 23:09:10 +0530

    added

---
+++ 1-Hello-world/hello-world.rs
@@ -0,0 +1,11 @@
+ /* Macros
+   println!("Hello World!"); --> macro: println!
+
+
+   macro --  A macro takes some input, and generates valid Rust code before your program is compiled.
+*/
+
+fn main() {
+    println!("Hello World!");
+    println!("I'm a Rustacean!");
+}
