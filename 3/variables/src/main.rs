d570f4ca6632ff81584a2a10a7a1166b77d1a8ba
santhoshini <santhoshinia01@gmail.com>
2026-04-08 23:42:48 +0530

    added

--- 3/variables/src/main.rs
+++ 3/variables/src/main.rs
@@ -5,5 +5,19 @@
     println!("The value of x is :{x}");
  }
 
-
+//constants
    
+const THREE_HOURS_IN_SECONDS :U32 = 60*60*3;
+
+//shadowing
+fn main(){
+
+   let x = 5;
+   let x = x+1;
+   {
+      let x = x*2;
+      println!("the value of x inner scope  :{x}");
+   }
+      println!("the value of x  :{x}");
+
+}
